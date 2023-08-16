996df8a5618c6a3d037518ddf62ecf4632c4e5a6
enterspacebs <anotherhell48@gmail.com>
2023-08-16 18:16:44 +0900

    0816 장고 입문 시작

--- reviews/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-
-# Register your models here.
-@admin.register(Review)
-class FeedAdmin(admin.ModelAdmin):
-    list_display = ("content", "created", "updated")
